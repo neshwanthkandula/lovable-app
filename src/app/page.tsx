e1d89768e5cb3ada3eacbdd80c8731b507258eb5
neshwanthkandula <kandulaneshwanth@gmail.com>
2025-11-21 07:38:32 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div >
+      <Button> click me</Button>
+      home page
+    </div>
+  )
+}
+
+export default page
